2b84a0b3cca369aa37e97d38468fc82acd56bad7
Gustavo Belisario <gbelisario@gmail.com>
2019-07-19 17:58:42 -0300

    ajuste debugbar

--- src/Http/Controllers/WebTinkerController.php
+++ src/Http/Controllers/WebTinkerController.php
@@ -9,11 +9,19 @@ class WebTinkerController
 {
     public function index()
     {
+        try {
+            \Debugbar::disable();
+        }
+        catch (\Exception $e){}
         return view('web-tinker::web-tinker');
     }
 
     public function execute(Request $request)
     {
+        try {
+            \Debugbar::disable();
+        }
+        catch (\Exception $e){}
         $validated = $request->validate([
             'code' => 'required',
         ]);
